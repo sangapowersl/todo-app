9e4d45a50c26bbdb349a73fc94764bcd67ccd6de
Thomas Sanga <sciencelearnia@gmail.com>
2023-07-29 16:36:06 +0200

    Update

--- src/components/TodoEditForm.js
+++ src/components/TodoEditForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function TodoEditForm({editTodo, task }) {
-    const [value, setValue] = useState("")
+    const [value, setValue] = useState(task.task)
 
     const handleSubmit = e => {
         e.preventDefault();
